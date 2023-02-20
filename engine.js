5bef00f6bfd1d27f5eedfc429ba18ffa6ce09cce
MaikolDaSilva <maikol.correia-da-silva@cpnv.ch>
2023-02-20 16:14:42 +0100

    Engine Action
    Ajout de la fonction qui gère la conséquence des actions

--- engine.js
+++ engine.js
@@ -29,17 +29,22 @@
 //Probabilité action
 //les actions: type 1= indiv, type 2= à deux, type=4 avec gagnant ou perdant 
 actions= [
-    {"ID":"ETU","type":1,"prob":[0, 2, 0, 0, 1],"effect":{"FC": 0,"CP":2,"RA":0,"RP":0,"BE":0,"RE":0}},
-    {"ID":"TRA","type":1,"prob":[0, 2, 0, 1, 0],"effect":{"FC": 0,"CP":1,"RA":100,"RP":0,"BE":0,"RE":1}},
+    {"ID":"ETU","type":1,"prob":[0, 2, 0, 0, 1],"effect":{"FC": 0,"CP":2,"RA":0,"RP":0,"BE":0,"RE":1}},
+    {"ID":"TRA","type":1,"prob":[0, 2, 0, 1, 1],"effect":{"FC": 0,"CP":1,"RA":1000,"RP":0,"BE":0,"RE":1}},
     {"ID":"JOS","type":1,"prob":[2, 0, -1, 0, 0],"effect":{"FC": 0,"CP":0,"RA":0,"RP":0,"BE":2,"RE":0}},
-    {"ID":"SPS","type":1,"prob":[1, 2, 0, 0, 0],"effect":{"FC": 2,"CP":0,"RA":0,"RP":0,"BE":1,"RE":0}},
-    {"ID":"VOS","type":1,"prob":[2, 0, 0, 0, -2],"effect":{"FC": 0,"CP":0,"RA":1,"RP":2,"BE":0,"RE":-1}},
-    {"ID":"COS","type":1,"prob":[2, 0, 0, 0, -2],"effect":{"FC": -1,"CP":-1,"RA":-2,"RP":0,"BE":1,"RE":0}},      
+    {"ID":"SPS","type":1,"prob":[0, 1, 0, 0, 0],"effect":{"FC": 2,"CP":0,"RA":0,"RP":0,"BE":1,"RE":0}},
+    {"ID":"VOS","type":1,"prob":[1, -1, 0, 2, -2],"effect":{"FC": 0,"CP":0,"RA":200,"RP":2,"BE":0,"RE":-1}},
+    {"ID":"COS","type":1,"prob":[2, 0, 0, 0, -2],"effect":{"FC": -1,"CP":-1,"RA":-500,"RP":0,"BE":1,"RE":0}},      
+    {"ID":"COB","type":1,"prob":[1, 0, 0, 2, 0],"effect":{"FC": 0,"CP": 0,"RA":-200,"RP":1,"BE":1,"RE":0}}, 
     
-    {"ID":"JO2","type":2,"prob":[1, 0, 1, 0, 0],"effect":{"FC": 0,"CP":0,"RA":0,"RP":0,"BE":2,"RE":2}},
-    {"ID":"DI2","type":2,"prob":[1, 0, 1, 0, 0],"effect":{"FC": 0,"CP":1,"RA":0,"RP":0,"BE":1,"RE":2}},
+    {"ID":"JO2","type":2,"prob":[1, 0, 2, 0, 0],"effect":{"FC": 0,"CP":0,"RA":0,"RP":0,"BE":2,"RE":2}},
+    {"ID":"DI2","type":2,"prob":[1, 0, 2, 0, 0],"effect":{"FC": 0,"CP":1,"RA":0,"RP":0,"BE":1,"RE":2}},
     {"ID":"SP2","type":2,"prob":[1, 1, 1, 0, 0], "effect":{"FC": 2,"CP":0,"RA":0,"RP":0,"BE":1,"RE":1}},//ici les deux sont gagnants
-    {"ID":"VO2","type":4,"prob":[2, 0, -1, 0, -1], "effect":{"FC": 0,"CP":0,"RA":0,"RP":1,"BE":0,"RE":-1}}//ici le premier est le gagnant, le second le perdant
+    {"ID":"VO2","type":4,"prob":[1, -1, 0, 2, -2], "effect":[{"FC": 0,"CP":0,"RA":200,"RP":1,"BE":1,"RE":0},{"FC": 0,"CP":0,"RA":200,"RP":1,"BE":1,"RE":-2},{"FC": 0,"CP":0,"RA":0,"RP":0,"BE":-1,"RE":0},{"FC": 0,"CP":0,"RA":0,"RP":0,"BE":-2,"RE":-2}]},//ici le premier est le gagnant 
+    // effect[0] vole et pas attrapé // effect[1] vole et attrapé // effect[2] vole pas et pas attrapé // effect[3] vole pas et attrapé 
+
+    {"ID":"VO2b","type":4,"effect":[{"FC": 0,"CP":0,"RA":-200,"RP":-1,"BE":-1,"RE":0},{"FC": 0,"CP":0,"RA":-200,"RP":-1,"BE":-1,"RE":-1},{"FC": 0,"CP":0,"RA":0,"RP":0,"BE":0,"RE":0},{"FC": 0,"CP":0,"RA":0,"RP":0,"BE":2,"RE":0}]}//ici le second le perdant   
+    // effect[0] se fait voler et voleur pas attrapé // effect[1] se fait voler et voleur attrapé // effect[2] se fait pas voler et voleur pas attrapé // effect[3] se fait pas voler et voleur attrapé 
 ]
  
 
@@ -56,7 +61,7 @@ const valDefStatus = 50;
    FC = Force, CP = Compétences, RA = Richesse sous forme argent, RP = Richesse sous forme possession, BE = bien-être, o	RE = qualité/quantité de relations
  */
 let creatureDef = [
-{   "ID": 0, "name": "creature1", 
+{   "ID": 0, "name": "creature 1", 
     "position" : {"x": 0, "z": 0}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -66,7 +71,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 1, "name": "creature2", 
+{   "ID": 1, "name": "creature 2", 
     "position" : {"x": 10, "z": 10}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -76,7 +81,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 2, "name": "creature3", 
+{   "ID": 2, "name": "creature 3", 
     "position" : {"x": 20, "z": 20}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -86,7 +91,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 3, "name": "creature4", 
+{   "ID": 3, "name": "creature 4", 
     "position" : {"x": 30, "z": 30}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -96,7 +101,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 4, "name": "creature5", 
+{   "ID": 4, "name": "creature 5", 
     "position" : {"x": 30, "z": 29}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -106,7 +111,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 5, "name": "creature6", 
+{   "ID": 5, "name": "creature 6", 
     "position" : {"x": 34, "z": 34}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -116,7 +121,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 6, "name": "creatur7", 
+{   "ID": 6, "name": "creature 7", 
     "position" : {"x": 35, "z": 35}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -126,7 +131,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 7, "name": "creature8", 
+{   "ID": 7, "name": "creature 8", 
     "position" : {"x": 25, "z": 25}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -136,7 +141,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 8, "name": "creature9", 
+{   "ID": 8, "name": "creature 9", 
     "position" : {"x": 15, "z": 15}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -146,7 +151,7 @@ let creatureDef = [
     "near"     : null,
     "action"   : null
 },
-{   "ID": 9, "name": "creature10", 
+{   "ID": 9, "name": "creature 10", 
     "position" : {"x": 5, "z": 5}, 
     "profile"  : [0.15,   0.15,   0.15,   0.15,   0.15],
     "status"   : {"FC": valDefStatus, "CP": valDefStatus, "RA": valDefStatus, "RP": valDefStatus, "BE": valDefStatus, "RE": valDefStatus},
@@ -165,7 +170,7 @@ let creatureTotal = creatureDef;
 for(let i = (creatureDef.length);i < 100; i++) {
     creatureTotal.push({
     "ID"       : i,
-    "name"     : "creature" + (i+1), 
+    "name"     : "creature " + (i+1), 
     "position" : {"x": Math.floor(Math.random() * 40), "z": Math.floor(Math.random() * 40)}, 
     "profile"  : [(Math.random() * 1.01).toFixed(2),   (Math.random() * 1.01).toFixed(2),
                   (Math.random() * 1.01).toFixed(2),   (Math.random() * 1.01).toFixed(2),   (Math.random() * 1.01).toFixed(2)],
@@ -282,6 +287,7 @@ function fnMove() {
     }     
     fnCheckPosOtherCreatures();//maikol 
     fnActionProba();//maikol 
+    fnActionEffect();//maikol
 }
 
 function fnChangeDirection (){ //pour changer le direction
@@ -364,12 +370,13 @@ function fnActionProba(){//pour calculer des probabilité des chaques actions
         tab.push({"nom" : "SPS", "prob" : fnAddProba(actions[3].prob, i), "type" : actions[3].type})
         tab.push({"nom" : "VOS", "prob" : fnAddProba(actions[4].prob, i), "type" : actions[4].type})
         tab.push({"nom" : "COS", "prob" : fnAddProba(actions[5].prob, i), "type" : actions[5].type})
+        tab.push({"nom" : "COB", "prob" : fnAddProba(actions[6].prob, i), "type" : actions[6].type})
 
         if (creatureTotal[i].near != null){
-            tab.push({"nom" : "JO2", "prob" : fnAddProba(actions[6].prob, i), "type" : actions[6].type})
-            tab.push({"nom" : "DI2", "prob" : fnAddProba(actions[7].prob, i), "type" : actions[7].type})
-            tab.push({"nom" : "SP2", "prob" : fnAddProba(actions[8].prob, i), "type" : actions[8].type})
-            tab.push({"nom" : "VO2", "prob" : fnAddProba(actions[9].prob, i), "type" : actions[9].type})
+            tab.push({"nom" : "JO2", "prob" : fnAddProba(actions[7].prob, i), "type" : actions[7].type})
+            tab.push({"nom" : "DI2", "prob" : fnAddProba(actions[8].prob, i), "type" : actions[8].type})
+            tab.push({"nom" : "SP2", "prob" : fnAddProba(actions[9].prob, i), "type" : actions[9].type})
+            tab.push({"nom" : "VO2", "prob" : fnAddProba(actions[10].prob, i), "type" : actions[10].type})
         }
 
         let ii = fnTire(tab);
@@ -377,9 +384,13 @@ function fnActionProba(){//pour calculer des probabilité des chaques actions
         if (tab[ii].type == 1){
             creatureTotal[i].action = tab[ii].nom;
         }
-        else{
+        else if (tab[ii].type == 2){
             creatureTotal[i].action = tab[ii].nom;
             creatureTotal[creatureTotal[i].near].action = tab[ii].nom;
+        }
+        else{
+            creatureTotal[i].action = tab[ii].nom;
+            creatureTotal[creatureTotal[i].near].action = tab[ii].nom+"b";
         }
     }    
 }
@@ -407,4 +418,92 @@ function fnTire(t){
     }
 }
 
+
+function fnActionEffect(){
+    let currentActionArray;
+    let effectArray;
+    let steal = 0;
+    for (let i = 0; i < creatureTotal.length; i++){
+        let currentAction = creatureTotal[i].action
+
+        if (currentAction == "VO2b"){
+            continue;
+        }
+
+        for (let j = 0; j < actions.length; j++){
+            if (currentAction == actions[j].ID){
+                currentActionArray = actions[j]
+                break;
+            }
+        }
+
+        if (currentActionArray.type == 4){
+ 
+            if (creatureTotal[i].status.FC > creatureTotal[creatureTotal[i].near].status.FC){//true si le voleur à plus de force que la victime
+                steal = 0; //voleur vole
+            }
+            else{
+                steal = 2;//voleur vole pas
+            }
+
+            let probability = Math.floor(Math.random() * 2) //définit si attrapé ou non
+            if(probability == 0 ) {
+                steal++;// voleur attrapé
+            }
+
+            effectArray = actions[11].effect[steal];
+            //conséquence pour la victime
+            creatureTotal[creatureTotal[i].near].status.FC += effectArray.FC;
+            creatureTotal[creatureTotal[i].near].status.CP += effectArray.CP;
+            creatureTotal[creatureTotal[i].near].status.RA += effectArray.RA;
+            if (creatureTotal[creatureTotal[i].near].status.RA < 0){
+                creatureTotal[creatureTotal[i].near].status.RA = 0;
+            }
+            creatureTotal[creatureTotal[i].near].status.RP += effectArray.RP;
+            if (creatureTotal[creatureTotal[i].near].status.RP < 0){
+                creatureTotal[creatureTotal[i].near].status.RP = 0;
+            }
+            creatureTotal[creatureTotal[i].near].status.BE += effectArray.BE;
+            if (creatureTotal[creatureTotal[i].near].status.BE < 0){
+                creatureTotal[creatureTotal[i].near].status.BE = 0;
+            }
+            creatureTotal[creatureTotal[i].near].status.RE += effectArray.RE;
+
+            effectArray = currentActionArray.effect[steal]; //conséquence pour le voleur
+
+
+        }
+
+        else{
+            effectArray = currentActionArray.effect;
+        }
+
+        creatureTotal[i].status.FC += effectArray.FC;
+        if (creatureTotal[i].status.FC < 0){
+            creatureTotal[i].status.FC = 0;
+        }
+        creatureTotal[i].status.CP += effectArray.CP;
+        if (creatureTotal[i].status.CP < 0){
+            creatureTotal[i].status.CP = 0;
+        }
+        creatureTotal[i].status.RA += effectArray.RA;
+        if (creatureTotal[i].status.RA < 0){
+            creatureTotal[i].status.RA = 0;
+        }
+        creatureTotal[i].status.RP += effectArray.RP;
+        if (creatureTotal[i].status.RP < 0){
+            creatureTotal[i].status.RP = 0;
+        }
+        creatureTotal[i].status.BE += effectArray.BE;
+        if (creatureTotal[i].status.BE < 0){
+            creatureTotal[i].status.BE = 0;
+        }
+        creatureTotal[i].status.RE += effectArray.RE;
+        if (creatureTotal[i].status.RE < 0){
+            creatureTotal[i].status.RE = 0;
+        }
+
+    }
+
+}
 
