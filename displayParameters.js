ee9611e524118e9b202a30d1af8bd98af95bdce1
AntoineMartet <antoine.martet@cpnv.ch>
2023-03-20 14:57:15 +0100

    Display : BC et BL dans Courbes.html

---
+++ displayParameters.js
@@ -0,0 +1,3 @@
+let legendsStatus = ["FC", "CP", "RA", "RP", "BC", "BL", "RE"]; // légendes à afficher
+let colors = ["yellow", "orange", "red", "purple", "blue", "darkblue", "green"]; // couleurs
+let legendsPersonality = ["I", "F", "R", "P", "E"]; // légendes à afficher
