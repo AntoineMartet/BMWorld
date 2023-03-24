a034e530d6aa9276f0fa3a3668bb9aa81f3fe352
AntoineMartet <antoine.martet@cpnv.ch>
2023-03-24 17:20:19 +0100

    Display : 3e camera + courbes.html réorganisé

--- displayParameters.js
+++ displayParameters.js
@@ -1,3 +1,7 @@
-let legendsStatus = ["FC", "CP", "RA", "RP", "BC", "BL", "RE"]; // légendes à afficher
-let colors = ["yellow", "orange", "red", "purple", "blue", "darkblue", "green"]; // couleurs
-let legendsPersonality = ["I", "F", "R", "P", "E"]; // légendes à afficher
+let statusLegends = ["FC", "CP", "RA", "RP", "BC", "BL", "RE"]; // légendes à afficher
+let statusColors = ["yellow", "orange", "red", "purple", "blue", "deepskyblue", "green"]; // couleurs
+
+let actionsLegends=["ETU","TRA","JOS","SPS","VOS","COS","COB","JO2","DI2","SP2","VO2","VO2b"];
+let actionsColors=["red","orange","yellow","lightgreen","green","darkgreen","darkblue","lightblue","blue","aqua","purple","pink",""];
+
+let personalityLegends = ["I", "F", "R", "P", "E"]; // légendes à afficher
